e2e42181b12813b839df9d00335910baeae534e2
panky98 <64682397+panky98@users.noreply.github.com>
2021-02-10 20:11:06 +0100

    Revert "Dodat endregion"
    This reverts commit 47daa032d9a4a120a7f80ffe7319e4460b364931.

--- MongoDB_BE/DataLayer/DataProvider.cs
+++ MongoDB_BE/DataLayer/DataProvider.cs
@@ -303,8 +303,6 @@ namespace DataLayer
 
             db.GetCollection<Proizvod>("proizvodi").UpdateOne(filter, update);
         }
-        #endregion
-
         #region Let
         public static void KreirajLet(Let let)
         {
